d7b2fa03e993b2951343761f2af23359ad989ec3
zhouyrt <zhouyrt@163.com>
2012-10-16 23:14:34 +0800

    view.html

--- backbone/dialog.js
+++ backbone/dialog.js
@@ -1,7 +1,21 @@
 ~function(){
 	
-	var model = new Backbone.Model()
+	var Model = Backbone.Model.extend({
+		caption: '对话框'
+	})
 	
-	var View = Backbone.View.extend()
+	var View = Backbone.View.extend({
+		model: new Model,
+		el: 'body',
+		template: _.template($('#dialog-template').html()),
+		render: function() {
+			$(this.el).html(this.template(this.model.toJSON()))
+		}
+	})
+	
+	
+	$(function() {
+		new View
+	})
 	
 }()
