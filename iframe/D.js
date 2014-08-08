9661beb354b459a1851508739d1c9f17d348d4a7
zhouyrt <zhouyrt@163.com>
2014-08-08 19:38:54 +0800

    s

--- iframe/D.js
+++ iframe/D.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var doc = document
     // var height = Math.max(document.documentElement.clientHeight, document.body.clientHeight)
-    var height = Math.max(doc.documentElement.clientHeight, doc.body.clientHeight) +  Math.max(doc.documentElement.scrollTop, doc.body.scrollTop)
+    var height = Math.max(doc.documentElement.clientHeight, doc.body.clientHeight, doc.documentElement.scrollHeight, doc.body.scrollHeight)
     var myifr = document.getElementById('myifr')
     if (myifr) {
         myifr.src = 'http://snandy.github.io/lib/iframe/B.html?height=' + height
