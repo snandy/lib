e06d26d1db4550d0df5b194bc19fc266fe1b051c
zhouyrt <zhouyrt@163.com>
2014-08-08 19:10:54 +0800

    s

--- iframe/D.js
+++ iframe/D.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var height = Math.max(document.documentElement.clientHeight, document.body.clientHeight)
     var myifr = document.getElementById('myifr')
     if (myifr) {
-        myifr.src = 'http://zgit.com/lib/i.html?height=' + height
+        myifr.src = 'http://snandy.github.io/lib/iframe/B.html?height=' + height
         console.log(height)        
     }
 };
