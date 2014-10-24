f70aabf44cee3ec7396dfdad5f37c676d3de668b
zhouyrt <zhouyrt@163.com>
2014-10-24 15:35:17 +0800

    s

--- marquee/carousel.js
+++ marquee/carousel.js
@@ -11,8 +11,15 @@ $.fn.carousel = function(options, callback) {
         speed: 300,
         autoPlay: true,
         stay: 5000,
-        navItems: false,
-        navItemsWrap: '.nav-wrap'
+        itemWrap: '.slider-wraper',
+        itemSel: '.slider-item',
+        navWrap: '.slider-nav-wrap',
+        nav: '.slider-nav',
+        navTpl: '<li></li>',
+        navCls: 'slider-nav',
+        navActiveCls: 'cur',
+        btnPrev: '.slider-btn-prev',
+        bntNext: '.slider-btn-next'
     }
 
     var settings = $.extend(defaults, options)
@@ -23,93 +30,114 @@ $.fn.carousel = function(options, callback) {
         var step = settings.step
 
         // some dom
-        var $main = $that.find('.slider-scroll')
-        var $item = $that.find('.slider-main-img')
-        var $navWrap = $that.find(settings.navItemsWrap)
-        var hasNavWrap = $navWrap.length > 0
-        var navItems = settings.navItems
-        var navItemActived = settings.navItemActived
-        var navItemEvent = settings.navItemEvent      
+        var nav = settings.nav
+        var navCls = settings.navCls
+        var navTpl = settings.navTpl
+        var navActiveCls = settings.navActiveCls
+        var $wraper = $that.find(settings.itemWrap)
+        var $items = $that.find(settings.itemSel)
+        var $navWrap = $that.find(settings.navWrap)
+        var $nav = $that.find(nav)
+        var hasNav = $navWrap.length > 0
+        var navItemEvent = settings.navItemEvent
+        var $btnPrev = $that.find(settings.btnPrev)
+        var $bntNext = $that.find(settings.bntNext)
 
         // some timer
         var timer = null
         var current = 1
-        var total = Math.ceil(($item.length - visible) / step) + 1 
-        var width = $item.outerWidth()
+        var total = Math.ceil(($items.length - visible) / step) + 1 
+        var itemWidth = $items.outerWidth()
 
-        $main.width(width*settings.visible)
-        $item.slice(0, settings.step).each(function(i, el) {
+        $wraper.width(itemWidth*settings.visible)
+        $items.slice(0, settings.step).each(function(i, el) {
             $(el).css({
                 position: 'absolute',
                 top: '0px',
-                left: i * width + 'px'
-            }).addClass('current')
+                left: i * itemWidth + 'px'
+            }).addClass('cur')
         })
 
-        if (settings.navItems) {
+        if (hasNav) {
             addNavItem()
         }
         
         function addNavItem() {
-            $navWrap = hasNavWrap ? $navWrap : $('<div class="' + settings.navItmesWrap + '"></div>')
+            $navWrap = hasNav ? $navWrap : $('<ul class="' + navCls + '"></ul>')
             for (var i = 0; i < total; i++) {
-                var $nav = $.isFunction(navItems) ? navItems(i) : $('<li></li>').text(i+1)
+                var $el = $.isFunction(navTpl) ? navTpl(i) : $(navTpl).addClass(navCls).text(i+1)
                 if (i === 0) {
-                    $nav.addClass(navItemActived)
+                    $el.addClass(navActiveCls)
                 }
-                $navWrap.append($nav)
+                $navWrap.append($el)
             }
-            if(!hasNavWrap) {
+            if(!hasNav) {
                 $that.after($navWrap)
-            }            
+            }
+            $nav = $navWrap.find(nav)
+        }
+
+        function setCurNav(i) {
+            if (i == 6) i = 0
+            if (hasNav) {
+                $nav.removeClass(navActiveCls)
+                $nav.eq(i).addClass(navActiveCls)
+            }
+        }
+        function switchTo() {
+            var $will = $items.slice(step * current, step * (current+1))
+            if (current == total) {
+                $will = $items.slice(0, step)
+                $items.slice(step * (total-1), step*total).each(function(i, el) {
+                    $(el).css({
+                        left: i * itemWidth + 'px'
+                    })                    
+                })
+            }
+            $will.addClass('cur').show()
+            $will.each(function(i, el) {
+                var left = itemWidth * (step + i)
+                $(el).css({
+                    position: 'absolute',
+                    top: '0px',
+                    left: left + 'px'
+                })
+            })
+            $wraper.css({
+                width: (itemWidth * step * 2) + 'px'
+            }).animate({
+                left: (-itemWidth * step) + 'px'
+            }, {
+                duration: settings.speed,
+                complete: function() {
+                    $wraper.css({
+                        width: itemWidth * step +'px',
+                        left: '0px'
+                    })
+                    var $prev = $items.slice(step*(current-1), step*current)
+                    $prev.removeClass('cur')
+                    $prev.hide()
+                    $will.each(function(i, el) {
+                        var left = itemWidth * i
+                        $(el).css({
+                            left: left + 'px'
+                        })
+                    })
+
+                    // 设置导航数字
+                    if (hasNav) setCurNav(current)
+
+                    current++
+                    if (current == total+1) {
+                        current = 1
+                    }
+                }
+            })
         }
 
         function play() {
             timer = setInterval(function() {
-                var $will = $item.slice(step * current, step * (current+1))
-                if (current == total) {
-                    $will = $item.slice(0, step)
-                    $item.slice(step * (total-1), step*total).each(function(i, el) {
-                        var width = $item.outerWidth()
-                        $(el).css({
-                            left: i * width + 'px'
-                        })                    
-                    })
-                }
-                $will.addClass('current')
-                $will.each(function(i, el) {
-                    var left = $item.width() * (step + i)
-                    $(el).css({
-                        position: 'absolute',
-                        top: '0px',
-                        left: left + 'px'
-                    })
-                })
-                $main.css({
-                    width: ($item.width() * step * 2) + 'px'
-                }).animate({
-                    left: (-$item.width() * step) + 'px'
-                }, {
-                    duration: settings.speed,
-                    complete: function() {
-                        $main.css({
-                            width: $item.width() * step +'px',
-                            left: '0px'
-                        })
-                        var $prev = $item.slice(step * (current-1), step*current)
-                        $prev.removeClass('current')
-                        $will.each(function(i, el) {
-                            var left = $item.width() * i
-                            $(el).css({
-                                left: left + 'px'
-                            })
-                        })
-                        current++
-                        if (current == total+1) {
-                            current = 1
-                        }
-                    }
-                })
+                switchTo()
             }, settings.stay)
         }
 
@@ -117,10 +145,31 @@ $.fn.carousel = function(options, callback) {
             clearInterval(timer)
         }
 
+        function addEvent() {
+            $that
+                .mouseenter(function() {
+                    stop()
+                })
+                .mouseleave(function() {
+                    if (settings.autoPlay) {
+                        play()
+                    }
+                })
+                .delegate(settings.btnPrev, 'click', function() {
+                    current--
+                    switchTo()
+                })
+                .delegate(settings.bntNext, 'click', function() {
+                    current++
+                    switchTo()
+                })
+        }
+
         if (settings.autoPlay) {
             play()
         }
         
+        addEvent()
     }
 
     return this.each(function() {
