73211290af810d3ea31e56e7967f72f69c8eb152
snandy <zhouyrt@gmail.com>
2015-10-10 10:14:10 +0800

    pagedrag

--- zepto/mo.js
+++ zepto/mo.js
@@ -2,7 +2,7 @@
 
 	var mo = {};
 
-
+	// 获取浏览器的样式前缀
 	var prefix = function() {
 		var styles = window.getComputedStyle(document.documentElement, '');
 		var core = (
@@ -12,6 +12,26 @@
 		    .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
 		)[1];
 		return '-' + core + '-';
+	}();
+
+	// 动态添加 css 样式
+	var addStyle = function() {
+		var styleSheet = function() {
+			var head = document.head;
+			var style = document.createElement('style'); 
+			style.type = 'text/css';
+			head.appendChild(style);
+			var styleSheets = document.styleSheets;
+			return styleSheets[styleSheets.length-1];
+		}();
+		return function(cssText) {
+			var cssRules = cssText.split('\r\n');
+			var len = !!styleSheet.cssRules ? styleSheet.cssRules.length : 0;
+			for (var i=0; i < cssRules.length; ++i) {
+				styleSheet.insertRule(cssRules[i],len++);
+			};
+			return len;
+		};
 	}();
 
 	var getTransformVal = function(transform, key, index) {
@@ -47,7 +67,9 @@
 	// console.log(s)
 
 	mo.prefix = prefix;
+	mo.addStyle = addStyle;
 	// console.log(prefix);
 	window.mo = mo;
+
 }(this, document);
 
